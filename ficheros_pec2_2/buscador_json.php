d53a949967c9522279acec5798f9e8d9a7ac432c
Jesus Gonzalez <chuso1982@gmail.com>
2025-04-11 05:42:45 +0200

    pequenas modificaciones

--- ficheros_pec2_2/buscador_json.php
+++ ficheros_pec2_2/buscador_json.php
@@ -1,13 +1,13 @@
 <?php
 require 'bd_pec2_2.php';
 
-// Invoco a la funcion de bd_pec2_2.php El resultado lo devuelvo en un json.
-// input buscardor llama al js, el js llama al buscador_json.php y este invoca a bd_pec2_2.php a la funcion buscar_productos con lo que le llegue en el GET
+// Invoco a la funcion buscar_producto() de bd_pec2_2.php El resultado lo devuelvo en un json.
+// input buscador llama al js, el js llama al buscador_json.php y este invoca a bd_pec2_2.php a la funcion buscar_productos con lo que le llegue en el GET
 // Y devolvemos el array en el buscador
 
-$resul = isset($_GET['resul']) ? $_GET['resul'] : '';
+$cadena = isset($_GET['cadena']) ? $_GET['cadena'] : '';
 
-$resultado = buscar_productos($resul);
+$resultado = buscar_productos($cadena);
 
 header('Content-Type: application/json');
 
